28065cc90b8d845724520d794ee1f37b747ba40a
HuyNX21 <nguyenxuanhuy9101@gmail.com>
2025-12-19 11:32:01 +0700

    add select mode

--- boardsellect.cpp
+++ boardsellect.cpp
@@ -7,15 +7,23 @@ BoardSellect::BoardSellect(QWidget* parent)
     : QWidget(parent)
 {
     QGridLayout* grid = new QGridLayout(this);
-    grid->setSpacing(0);
+    grid->setSpacing(5);
     grid->setContentsMargins(0, 0, 0, 0);
 
-    for (int i = 0; i < 4; ++i)
-    {
-        QPushButton* btn = new QPushButton(QString::number(i + 1), this);
-        btn->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-        grid->addWidget(btn, i / 2, i % 2);
-    }
+    QPushButton* btn1 = new QPushButton(tr("8x8"), this);
+    QPushButton* btn2 = new QPushButton(tr("16x16"), this);
+    QPushButton* btn3 = new QPushButton(tr("30x16"), this);
+    QPushButton* btn4 = new QPushButton(tr("?"), this);
+
+    btn1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    btn2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    btn3->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    btn4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    grid->addWidget(btn1, 0, 0);
+    grid->addWidget(btn2, 0, 1);
+    grid->addWidget(btn3, 1, 0);
+    grid->addWidget(btn4, 1, 1);
 
     grid->setRowStretch(0, 1);
     grid->setRowStretch(1, 1);
